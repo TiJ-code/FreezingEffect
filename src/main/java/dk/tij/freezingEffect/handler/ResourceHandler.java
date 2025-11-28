49ceef8b8bd0a086fd449611dafc0e7cba882998
TιJ <78140568+TiJ-code@users.noreply.github.com>
2025-11-28 20:20:56 +0100

    Refactored Isolation Code & Added Insurance
    Ensuring that the item defined in the isolation configuration is an as
    armour wearable item.
    
    Refactored the code to have less indentation.

--- src/main/java/dk/tij/freezingEffect/handler/ResourceHandler.java
+++ src/main/java/dk/tij/freezingEffect/handler/ResourceHandler.java
@@ -2,6 +2,7 @@ package dk.tij.freezingEffect.handler;
 
 import dk.tij.freezingEffect.constants.InterpolationFunctions;
 import dk.tij.freezingEffect.constants.TemperatureConstants;
+import dk.tij.freezingEffect.utils.ItemUtils;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -21,32 +22,10 @@ public class ResourceHandler {
     }
 
     public void loadConfig() {
-        ConfigurationSection isolationSection = config.getConfigurationSection("frost.isolation");
-        if (isolationSection != null) {
-            int maxPossibleIsolationValue = InterpolationFunctions.clampI(
-                    isolationSection.getInt("maxPossibleIsolation", 0),
-                    0, 100
-            );
-            TemperatureConstants.MAX_POSSIBLE_ISOLATION = maxPossibleIsolationValue * TO_PERCENT_CONVERSION_FACTOR;
-
-            ConfigurationSection armourSection = isolationSection.getConfigurationSection("armourPieces");
-            if (armourSection != null) {
-                for (String key : armourSection.getKeys(false)) {
-                    Material material = Material.getMaterial(key);
-                    if (material != null) {
-                        int value = InterpolationFunctions.clampI(
-                                armourSection.getInt(key),
-                                0, 100
-                        );
-                        TemperatureConstants.ARMOUR_PIECE_ISOLATION.put(material,
-                                                                        value * TO_PERCENT_CONVERSION_FACTOR);
-                    }
-                }
-            }
-        }
-
         TemperatureConstants.CRITICAL_FREEZING_TICKS = config.getInt("frost.criticalFreezingTicks", Integer.MAX_VALUE);
         TemperatureConstants.HEAT_RADIUS = config.getInt("frost.heatRadius", 0);
+
+        loadIsolationValues();
 
         Set<Material> configHeatSources = config.getStringList("frost.heatSources")
                 .stream()
@@ -57,5 +36,34 @@ public class ResourceHandler {
         String interpolationFunctionName = config.getString("frost.interpolationFunction",
                 TemperatureConstants.INTERPOLATION_FUNCTIONS_MAPPING.keySet().toArray(String[]::new)[0]);
         TemperatureConstants.INTERPOLATION_FUNCTION = TemperatureConstants.INTERPOLATION_FUNCTIONS_MAPPING.get(interpolationFunctionName);
+    }
+
+    private void loadIsolationValues() {
+        ConfigurationSection isolationSection = config.getConfigurationSection("frost.isolation");
+
+        if (isolationSection == null) return;
+
+        int maxPossibleIsolationValue = InterpolationFunctions.clampI(
+                isolationSection.getInt("maxPossibleIsolation", 0),
+                0, 100
+        );
+        TemperatureConstants.MAX_POSSIBLE_ISOLATION = maxPossibleIsolationValue * TO_PERCENT_CONVERSION_FACTOR;
+
+        ConfigurationSection armourSection = isolationSection.getConfigurationSection("armourPieces");
+
+        if (armourSection == null) return;
+
+        for (String key : armourSection.getKeys(false)) {
+            Material material = Material.getMaterial(key);
+
+            if (!ItemUtils.isArmourItem(material)) continue;
+
+            int value = InterpolationFunctions.clampI(
+                    armourSection.getInt(key),
+                    0, 100
+            );
+            TemperatureConstants.ARMOUR_PIECE_ISOLATION.put(material,
+                    value * TO_PERCENT_CONVERSION_FACTOR);
+        }
     }
 }
